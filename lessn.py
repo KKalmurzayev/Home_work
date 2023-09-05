b6f9a5b7967b8e9710042eb238cc607d57875a7c
Калмурзаев Кайрат Абимолдаевич <KalmurzaevK@lcoast.itstep>
2023-09-05 21:49:17 +0600

    adad

--- lessn.py
+++ lessn.py
@@ -1,10 +1,12 @@
 import math
 
 list = []
-'''z = [(lambda x: math.sin(x))(i) for i in range(91)]
+z = [(lambda x: math.sin(x))(i) for i in range(91)]
 k = math.degrees(z)
-print(k)'''
+print(k)
 
+k=10
+#конвертация с радиана в градусыпуе
 z = [(math.degrees(i)) for i in range(30)]
 deg = [(math.sin(i)) for i in z]
 print(deg)
